4dac79516e40935f4c74194ff5a0eb7e3462dfca
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-09-30 16:55:53 +0700

    Asking For number of Students

--- src/FinalExam.java
+++ src/FinalExam.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class FinalExam {
+    static Scanner scanner;
+    public static void main(String[] args) {
+        System.out.println("*****   Student Exam Result   *****");
+        System.out.println("Enter number of student : ");
+        int numOfStudent = scanner.nextInt();
+
+    }
 }
